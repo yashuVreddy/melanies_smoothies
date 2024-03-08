cea4ffdfca224cc482068cdb0e7b642d703fe39f
yashuVreddy <162623644+yashuVreddy@users.noreply.github.com>
2024-03-08 15:12:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!',icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
